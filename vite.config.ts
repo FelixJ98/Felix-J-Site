e49aece9510ea5939b169962605731c725447c8a
Felix Jarquin <145188084+FelixJ98@users.noreply.github.com>
2025-09-18 23:53:54 -0400

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    server: {
+        host: "::",
+        port: 8080,
     },
-      base: '/Felix-J-Site/'
-  },
-
+    base: '/Felix-J-Site/',
+    plugins: [
+        react(),
+        mode === 'development' &&
+        componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 }));
